da234bf902e6e0b4ca6e729ae0788d481c751082
mikeygelooooo <mikeygelo7@gmail.com>
2024-12-09 21:17:07 +0800

    Java Adventure Game v5.2 - Added Settings Configuration - Now Able to Save and Load Settings Configuration

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,10 +10,15 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("Pixel Odyssey");
-        // window.setUndecorated(true);
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
+
+        gamePanel.config.loadConfig();
+
+        if (gamePanel.fullScreenOn) {
+            window.setUndecorated(true);
+        }
 
         window.pack();
 
